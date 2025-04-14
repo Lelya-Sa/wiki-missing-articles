333c5a110a545ee2ee7f459733e8c2000dbcfbff
malakatshy <malakatshy100@gmail.com>
2025-04-14 19:24:59 +0300

    fixed articles urls TODO : - improve rating system. - list result data in table. - optimize running time. - make a better design of the page. -fix portal search ( not mandatory) making the page redirected to the translated selected language page ( not mandatory)

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-*8$^id4712p@mazemi%r*pn-avn-$n&$#oe^4#$4hlz&b6+p+r
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['*']
 
 # DEBUG = False
