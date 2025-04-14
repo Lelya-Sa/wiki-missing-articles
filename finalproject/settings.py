3b6306f04e8ce7a9868e079bd5efc23d1ba844c5
malakatshy <malakatshy100@gmail.com>
2025-04-14 17:18:40 +0300

    fixed articles urls TODO : - improve rating system. - list result data in table. - optimize running time. - make a better design of the page. -fix portal search ( not mandatory) making the page redirected to the translated selected language page ( not mandatory)

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = 'django-insecure-*8$^id4712p@mazemi%r*pn-avn-$n&$#oe^4#$4hlz&b6+p+r
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'multilingual-missing-articles.toolforge.org',
+]
 # ALLOWED_HOSTS = ['*']
 
 # DEBUG = False
